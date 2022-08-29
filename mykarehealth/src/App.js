7a6559e5b0bdf4873aed63b8c03a3c122a1b49e9
SajjuM <112389969+SajjuM@users.noreply.github.com>
2022-08-29 17:12:09 +0530

    Add files via upload

---
+++ mykarehealth/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Login from './components/Login';
+import {Routes, Route} from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Register from './components/Register'
+function App() {
+  return (
+    // <div className="App">
+    //   <Login/>
+    // </div>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/sign-up' element={<Register/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+    </Routes>
+  );
+}
+
+export default App;
